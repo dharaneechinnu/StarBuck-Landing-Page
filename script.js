621e5e6bdba2c4fb8ef8592c958c63851b484e2b
dharaneedharan <117936700+dharaneechinnu@users.noreply.github.com>
2024-03-03 16:40:27 +0530

    main

---
+++ script.js
@@ -0,0 +1,17 @@
+function imgslider(anything){
+    document.querySelector('.starbucks').src= anything;
+    
+}
+function changecolor(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
+
+function changetext(color){
+    const circle = document.querySelector('.star');
+    circle.style.color = color;
+}
+function changebtn(color){
+    const circle = document.querySelector('.btn');
+    circle.style.background = color;
+}
